c7a30dfefcc535b6026100adfd0075609d058291
Claudio Paloscia <claudioclaudio91@gmail.com>
2024-11-06 20:16:36 -0600

    Removed tracking of dist and fixed listening address

--- server/dist/src/routes/dashboardRoutes.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const dashboardController_1 = require("../controllers/dashboardController");
-const router = (0, express_1.Router)();
-// Everything here is at the /dashboard path
-router.get("/", dashboardController_1.getDashboardMetrics);
-exports.default = router;
